d96ce08e11379d7d338f3200dd567334ae7e35ba
Lucien <3dgobinlin@gmail.com>
2018-10-13 04:35:42 -0400

    Video 7: OpenGL Element Array Buffers

--- GraphicsPad/MeGlWindow.cpp
+++ GraphicsPad/MeGlWindow.cpp
@@ -20,24 +20,28 @@ void MeGlWindow::initializeGL()
 		-1.0f, +1.0f,
 		+1.0f, +1.0f,
 		+0.0f, -0.0f,
-		+0.0f, -0.0f,
 		+1.0f, -1.0f,
 		-1.0f, -1.0f,
 	};
-	GLuint myBufferID;
-	glGenBuffers(1, &myBufferID);
-	glBindBuffer(GL_ARRAY_BUFFER, myBufferID);
+	GLushort indices[] = { 0, 1, 2, 2, 3, 4 };
+	GLuint vertexBufferID;
+	glGenBuffers(1, &vertexBufferID);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(verts),
 				 verts, GL_STATIC_DRAW);
 	
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
 
+	GLuint indexBufferID;
+	glGenBuffers(1, &indexBufferID);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 }
 
 void MeGlWindow::paintGL()
 {
 	glViewport(0, 0, width(), height());
-	glDrawArrays(GL_TRIANGLES, 0, 6);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);
 }
 
