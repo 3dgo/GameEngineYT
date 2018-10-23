6ee87ba3579dee8adf2c2704505f72c1733c2b8d
Lucien <3dgobinlin@gmail.com>
2018-10-23 01:24:59 -0400

    Video 12: OpenGL Compiling GLSL Shaders

--- GraphicsPad/MeGlWindow.cpp
+++ GraphicsPad/MeGlWindow.cpp
@@ -1,6 +1,8 @@
 #include <GL\glew.h>
 #include "MeGlWindow.h"
 
+extern const char* vertexShaderCode;
+extern const char* fragmentShaderCode;
 
 MeGlWindow::MeGlWindow()
 {
@@ -11,17 +13,24 @@ MeGlWindow::~MeGlWindow()
 {
 }
 
-void MeGlWindow::initializeGL()
+void SendDataToOpenGL()
 {
-	glewInit();
-
 	GLfloat verts[] =
 	{
 		-1.0f, +1.0f,
+		+1.0f, +0.0f, +0.0f,
+
 		+1.0f, +1.0f,
+		+1.0f, +0.0f, +0.0f,
+
 		+0.0f, -0.0f,
+		+1.0f, +0.0f, +0.0f,
+
 		+1.0f, -1.0f,
+		+1.0f, +0.0f, +0.0f,
+
 		-1.0f, -1.0f,
+		+1.0f, +0.0f, +0.0f,
 	};
 	GLushort indices[] = { 0, 1, 2, 2, 3, 4 };
 	GLuint vertexBufferID;
@@ -29,14 +38,47 @@ void MeGlWindow::initializeGL()
 	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(verts),
 				 verts, GL_STATIC_DRAW);
-	
+
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 5, 0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (char*)(2 * sizeof(float)));
 
 	GLuint indexBufferID;
 	glGenBuffers(1, &indexBufferID);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices),
+				 indices, GL_STATIC_DRAW);
+}
+
+void InstallShaders()
+{
+	GLuint vertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+	GLuint fragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+
+	const char* adapter[1];
+	adapter[0] = vertexShaderCode;
+	glShaderSource(vertexShaderID, 1, adapter, 0);
+	adapter[0] = fragmentShaderCode;
+	glShaderSource(fragmentShaderID, 1, adapter, 0);
+
+	glCompileShader(vertexShaderID);
+	glCompileShader(fragmentShaderID);
+
+	GLuint programID = glCreateProgram();
+	glAttachShader(programID, vertexShaderID);
+	glAttachShader(programID, fragmentShaderID);
+	glLinkProgram(programID);
+
+	glUseProgram(programID);
+}
+
+void MeGlWindow::initializeGL()
+{
+	glewInit();
+	SendDataToOpenGL();
+	InstallShaders();
+
 }
 
 void MeGlWindow::paintGL()
